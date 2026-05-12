eb4df87c7c625c11a8c5076996d638f48083d489
Santiago Fernandez Somellera <a01646946@tec.mx>
2026-05-12 16:23:09 -0600

    chore: setup scalar api docs

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from fastapi import FastAPI
+from scalar_fastapi import get_scalar_api_reference
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+)
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.get('/scalar', include_in_schema=False)
+def get_docs_scalar():
+    return get_scalar_api_reference(
+        openapi_url=app.openapi_url,
+        title="Scalar API",
+    )
